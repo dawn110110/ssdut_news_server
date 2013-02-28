5c0fbd91254020e6e06f21afe6c565a80ced2a3e
dawn110110 <dawn110110@gmail.com>
2013-02-28 18:27:56 +0800

    BidDict表，和辅助访问的类（访问方式类似web.utils.Storage）

--- db.py
+++ db.py
@@ -17,4 +17,5 @@ Base.query = db_session.query_property()
 
 
 def init_db():
+    import models
     Base.metadata.create_all(bind=engine)
--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 from sqlalchemy import Column, Integer, String, Text, Date
+from sqlalchemy.orm.exc import NoResultFound
+import db
 from db import Base
+import logging
 
 __all__ = ['New']
 
@@ -19,3 +22,65 @@ class New(Base):
 
     def __unicode__(self):
         return self.__repr__()
+
+
+class BigDict(Base):
+    __tablename__ = 'bigdict'
+    id = Column(Integer, primary_key=True)
+    key = Column(String(200))
+    val = Column(String(1000))
+
+    def __repr__(self):
+        return '<Option %s : %s>' % (self.key, self.val)
+
+    def __unicode__(self):
+        self.__repr__()
+
+class KV_util(object):
+    ''' act like a dict, using BigDict
+    e.g:
+    KV[1] = 123
+    KV['hello'] = 'world'
+    print KV.hello
+    KV.hello = 'goodbye'
+    print KV.nokey
+    '''
+    def __getattr__(self, key):
+        try:
+            res = BigDict.query.filter(BigDict.key == key).one()
+            if res.val.isdigit():
+                return int(res.val)
+            else:
+                return res.val
+        except:
+            return u''
+    def __setattr__(self, key, val):
+        if not isinstance(val, int):
+            try:
+                val = str(val)
+            except:
+                logging.error("store %r in BigDict failed\
+                    when encode to utf-8" % val)
+        try:
+            q = BigDict.query.filter(BigDict.key == key)
+            q.one()
+            q.update({'val': val}, synchronize_session=False)
+        except NoResultFound:
+            record = BigDict(key=key, val=val)
+            db.ses.add(record)
+            db.ses.commit()
+        except Exception, e:
+            raise e
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            key = str(key)
+        return getattr(self, key)
+
+    def __setitem__(self, key, val):
+        if isinstance(key, int):
+            key = str(key)
+        setattr(self, key, val)
+
+KV = KV_util()
+kv = KV
